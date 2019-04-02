3af1fc4447809321e3e0ef577697193809a4eb5f
Elijah Sawyers <elijahsawyers@gmail.com>
2019-04-02 09:02:11 -0500

    Working on bettering the accuracy

--- Core Motion Location Services/ViewController.swift
+++ Core Motion Location Services/ViewController.swift
@@ -35,37 +35,59 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        cmManager.deviceMotionUpdateInterval = 0.01
+        // The time intervals between pulling accelerometer data.
+        let updateInterval = 0.01
+        
+        // The number of intervals to run before averaging the accelerations and updating the user displacement.
+        let numberOfIntervals = 100
+        
+        // Initialize the Core Motion package.
+        cmManager.deviceMotionUpdateInterval = updateInterval
         cmManager.startDeviceMotionUpdates()
         
+        // Keep track of which iteration we're currently on.
         var iter = 0
+        
+        // Store the average acceleration over the course of X amount of intervals.
         var avgAccel = 0.0
         
-        let timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { [weak self] timer in
+        let timer = Timer.scheduledTimer(withTimeInterval: updateInterval, repeats: true) { [weak self] timer in
+            // Grab the acceleration information.
             let deviceMotion = self!.cmManager.deviceMotion
-            var y = 0.0;
             
-            if let usrY = deviceMotion?.userAcceleration.y {
-                y = usrY * 9.81
+            // Phone acceleration in the "y" direction.
+            var yAccel = 0.0
+            
+            if let phoneYAccel = deviceMotion?.userAcceleration.y {
+                // Convert G's to m/s^2.
+                yAccel = phoneYAccel * 9.81
                 
-                // Values less than 0.3 are "garbage." (i.e. the user isn't moving)
-                if y > 0.5 {
-                    if iter >= 100 {
-                        iter = 0
-                        self!.currentAcceleration = (avgAccel/100.0)
-                        self!.currentVelocity = (avgAccel/100.0) * 1
+                // Values less than 0.3 are "junk values" (i.e. the user isn't moving).
+                if yAccel >= 0.30 {
+                    if iter >= numberOfIntervals {
+                        // Set the current acceleration and velocity.
+                        self!.currentAcceleration = (avgAccel/Double(numberOfIntervals))
+                        self!.currentVelocity = (avgAccel/Double(numberOfIntervals)) * updateInterval
+                        
+                        // Update user displacement.
                         self!.updateDisplacement(pV: self!.previousVelocity, cA: self!.currentAcceleration, t: 1.0)
+                        
+                        // Update previous velocity to be the current velocity.
                         self!.previousVelocity = self!.currentVelocity
+                        
+                        // Reset the itereration data.
+                        iter = 0
+                        //avgAccel = 0.0
                     } else {
+                        // Update the itereration data.
                         iter += 1
-                        avgAccel += y
+                        avgAccel += yAccel
                     }
                 }
             }
             
-            
             // Monitor acceleration.
-            self!.userAccel.text = "Y: " + String(y)
+            self!.userAccel.text = "Y: " + String(yAccel)
             
             // Monitor acceleration.
             self!.displacement.text = String(self!.userDisplacement)
