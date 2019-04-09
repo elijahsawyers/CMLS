e0f4588ccef62926dbe39d4a2542a29fccabe164
Elijah Sawyers <elijahsawyers@gmail.com>
2019-04-09 13:30:00 -0500

    Working on configuration

--- Core Motion Location Services/ViewController.swift
+++ Core Motion Location Services/ViewController.swift
@@ -25,23 +25,7 @@ class ViewController: UIViewController {
     let updateInterval = 0.01
     
     /// The user's average walking speed.
-    var averageVelocity: Double?
-    
-    /// The user's average walking velocities pulled during configuration.
-    var averageVelocities: [Double] = [] {
-        didSet {
-            if averageVelocities.count == 3 {
-                averageVelocity = (averageVelocities[0] + averageVelocities[1] + averageVelocities[2])/3.0
-                log.text += "Finished config with avg velocity: \(averageVelocity!)m/s \n"
-                
-                for i in 0..<3 {
-                    avgWMAs[i] /= 100
-                }
-                avgWMA = (avgWMAs[0] + avgWMAs[1] + avgWMAs[2])/3.0
-                log.text += "Finished config with avg wma: \(avgWMA!)m/s \n"
-            }
-        }
-    }
+    var averageVelocity = 0.0
     
     /// The average wma during configuration.
     var avgWMA: Double?
@@ -52,8 +36,9 @@ class ViewController: UIViewController {
     /// Is the user configuring the app?
     var configuring = false
     
-    /// The time for one configuration interval.
-    var configTime = 0.0
+    var configurationIterations = 0
+    
+    var configurationTime = 0.0
     
     /// Timer to configure the user's walking speed.
     var configurationTimer: Timer?
@@ -96,71 +81,49 @@ class ViewController: UIViewController {
     
     /// Used to start the configuration process.
     @IBAction func config(_ sender: UIButton) {
-        if !configuring {
-            log.text += "Walk 4 meters, and press the config button again when done!\n"
+        var v0 = 0.0
+        
+        var values: [Double] = Array(repeating: 0.0, count: 10)
+        
+        log.text = "Hold the phone flat in front of you, walk roughly 4 meters, and press the config button again when done!\n"
+        
+        // Pull accelerometer data for configuration.
+        configurationTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] timer in
+            self!.configurationIterations += 1
             
-            // Previous acceleration.
-            var previousAcceleration: Vector?
+            // Grab the motion information.
+            let accelerationData = self!.cmManager.deviceMotion?.userAcceleration
             
-            // To try to eliminate "noise," we use a moving average for the angle between current and previous acceleration vectors.
-            var thetaValues: [Double] = Array(repeating: 0.0, count: 100)
-            
-            // Pull accelerometer data for configuration.
-            configurationTimer = Timer.scheduledTimer(withTimeInterval: updateInterval, repeats: true) { [weak self] timer in
-                // Increase congig time each interval.
-                self!.configTime += self!.updateInterval
+            // Grab the acceleration vector.
+            if let y = accelerationData?.y {
+                // Convert G's to m/s^2.
+                let acceleration = y * 9.81
                 
-                // Grab the motion information.
-                let accelerationData = self!.cmManager.deviceMotion?.userAcceleration
+                values.append(acceleration)
+                values.removeFirst()
                 
-                // Grab the acceleration vector.
-                if let x = accelerationData?.x, let y = accelerationData?.y, let z = accelerationData?.z {
-                    // Convert G's to m/s^2, and assign it to a vector.
-                    let currentAcceleration = Vector(
-                        x * 9.81,
-                        y * 9.81,
-                        z * 9.81
-                    )
+                // Calculate the current velocity.
+                if self!.configurationIterations >= 10 {
+                    let vf = v0 + self!.weightedMovingAverage(values: values) * 0.1
                     
-                    // Calculate the angle between the current and previous acceleration vector.
-                    var theta: Double
-                    if previousAcceleration != nil {
-                        theta = currentAcceleration.dotProduct(vector: previousAcceleration!)
-                    } else {
-                        // Set current acceleration to the previous acceleration.
-                        previousAcceleration = currentAcceleration
-                        return
+                    if self!.configurationIterations == 40 {
+                        timer.invalidate()
+                        self!.displacement.text = String(vf)
+                        self!.averageVelocity += vf
+                        self!.configurationIterations = 0
+                        v0 = 0.0
+                        values = Array(repeating: 0.0, count: 10)
                     }
                     
-                    // Append the current angle and remove the oldest.
-                    thetaValues.removeFirst()
-                    thetaValues.append(theta)
-                    
-                    // Compute the average wma during the 1->2 seconds.
-                    if self!.configTime >= 1.0 && self!.configTime <= 2.0 {
-                        self!.avgWMAs[self!.averageVelocities.count] += abs(self!.weightedMovingAverage(values: thetaValues))
-                    }
-                    
-                    // Set current acceleration to the previous acceleration.
-                    previousAcceleration = currentAcceleration
+                    // Set the previous velocity as the current.
+                    v0 = vf
                 }
             }
-            
-            configuring = true
-        } else {
-            configurationTimer!.invalidate()
-            configurationTimer = nil
-            
-            log.text += "You walked \(4.0 / configTime)m/s!\n"
-            
-            averageVelocities.append(4.0 / configTime)
-            
-            configuring = false
-            configTime = 0.0
         }
     }
     
     @IBAction func walk(_ sender: Any) {
+        averageVelocity = 1.2
         stopLogging = !stopLogging
         
         if stopLogging {
@@ -179,10 +142,10 @@ class ViewController: UIViewController {
         // Previous acceleration.
         var previousAcceleration: Vector?
         
-        // Keep track of time above 0.8.
+        // Keep track of time above 0.75.
         var timeAbove = 0.0
         
-        // Has the wma been above 0.8 for more than one second?
+        // Has the wma been above 0.75 for more than one second?
         var timeAboveOneSec = 0.0
         
         // Walking?
@@ -221,7 +184,7 @@ class ViewController: UIViewController {
                 thetaValues.removeFirst()
                 thetaValues.append(theta)
                 
-                // Keep up with wma time above 0.8
+                // Keep up with wma time above 0.75.
                 if abs(self!.weightedMovingAverage(values: thetaValues)) > 0.8 {
                     timeAbove += self!.updateInterval
                     if timeAbove >= 1.0 {
@@ -247,11 +210,11 @@ class ViewController: UIViewController {
                         walking = true
                         
                         //...update displacement...
-                        self!.userDisplacement += self!.averageVelocity!
+                        self!.userDisplacement += self!.averageVelocity
                         
                         //...and start a timer to update displacement every 1.0s.
                         displacementTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
-                            self!.userDisplacement += self!.averageVelocity!
+                            self!.userDisplacement += self!.averageVelocity
                             self!.displacement.text = String(self!.userDisplacement)
                         }
                         displacementTimer!.fire()
